c53c33b64da04050dd1d3abedd6a0428aa633742
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-07-10 14:12:24 +0530

    commit

--- .vscode/polymorphism1.cpp
+++ .vscode/polymorphism1.cpp
@@ -2,6 +2,8 @@
 /*• Redefining a function of base class in derived class
 • Function overriding is used for achieving runtime polymorphism
 • Prototype of a overrides function must be exactly same as base class function*/
+// JUST LIKE IN FUNCTION OVERLOADING THE PROTOTYPE MUST REMAIN SAME , and then the new function called inside the child class will be called instead of parent class.
+
 #include<iostream>
 using namespace std;
 
@@ -18,7 +20,7 @@ public:
 class Derived: public Base
 {
 public:
- void fun()
+ void fun()//also cant provide argument here as the prototype should be exactly same
  {
  cout<<"fun of Derived"<<endl;
  }
