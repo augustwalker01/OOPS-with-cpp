374280d03815a6dd89e267e77e3fd0eb58678cd7
augustwalker01 <priyanshu.20scse1010511@galgotiasuniversity.edu.in>
2022-07-11 10:06:18 +0530

    commit

---
+++ .vscode/polymorphism3.cpp
@@ -0,0 +1,34 @@
+/*Polymorphism 
+• Same name different actions
+• Runtime Polymorphism is achieved using function overriding
+• Virtual functions are abstract functions of base class
+• Derived class must override virtual function
+• Base class pointer pointing to derived class object and a override function is called 
+Summary: class car is defined, then sub classes override, then base class method made as 
+virtual the pure virtual */
+#include<iostream>
+using namespace std;
+
+class Car
+{
+public:
+ virtual void start()=0;
+};
+class Innova:public Car
+{
+public:
+ void start(){cout<<"Innova Started"<<endl;}
+};
+class Swift:public Car
+{
+public:
+ void start(){cout<<"Swift Started"<<endl;}
+};
+int main()
+{
+ //Car c;
+ Car *p=new Innova();
+ p->start();
+ p=new Swift();
+ p->start();
+}
